f565cf357d38fe822c7523cbc5e6794edac96231
Naveen-Valmikam <naveen.valmikam@gmail.com>
2017-05-26 09:36:30 +1000

    set up app for Heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://NaveenValmikam:ILoveCoding123$@ds153521.mlab.com:53521/todo-app-api'
+};
+mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ var {User} = require('./models/user');
 var app = express();
 app.use(bodyParser.json());
 
+const port = process.env.PORT||3000;
+
 app.post('/todos',(req,res)=>{
   var todo = new Todo({
     text: req.body.text
@@ -50,8 +52,8 @@ app.get('/todos/:id',(req,res)=>{
   });
 });
 
-app.listen(3000,()=>{
-  console.log('Started on port :3000');
+app.listen(port,()=>{
+  console.log(`Started on port :${port}`);
 });
 
 module.exports ={app};
