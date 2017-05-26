c861cc8b4cb1e8dde60358855cc7b242d49d665a
Naveen-Valmikam <naveen.valmikam@gmail.com>
2017-05-26 10:48:07 +1000

    Changed Mongo DB Connection to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,10 @@
 var mongoose = require('mongoose');
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://NaveenValmikam:ILoveCoding123$@ds153521.mlab.com:53521/todo-app-api'
-};
-mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
+// let db = {
+//   localhost: 'mongodb://localhost:27017/TodoApp',
+//   mlab: 'mongodb://testuser:password1@ds153521.mlab.com:53521/todo-app-api'
+// };
+mongoose.connect( process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
