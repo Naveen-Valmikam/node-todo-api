51cadb168528bee344941301302bc3184d9575d0
Naveen-Valmikam <naveen.valmikam@gmail.com>
2017-05-26 14:57:14 +1000

    Added tests for PATCH /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos=[{
   text:'Learn Node.js'
 }, {
   _id:new ObjectID(),
-  text:'Practice Node'
+  text:'Practice Node',
+  completed:true,
+  completedAt:333
 }];
 
 
@@ -138,4 +140,58 @@ describe('DELETE /todos',()=>{
     .expect(404)
     .end(done);
   });
+});
+
+describe('PATCH /todos/:id',()=>{
+  it('should update a given todo.',(done)=>{
+      var id = todos[0]._id.toHexString();
+      var text = 'New text for the todo';
+
+      request(app)
+      .patch(`/todos/${id}`)
+      .send({
+        completed:true,
+        text
+      })
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed',(done)=>{
+    var id= todos[1]._id.toHexString();
+
+    request(app)
+    .patch(`/todos/${id}`)
+    .send({
+      completed:false
+    })
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todo.completed).toBe(false);
+      expect(res.body.todo.completedAt).toNotExist();
+    })
+    .end(done);
+  });
+
+  it('should return 404 if todo not found',(done)=>{
+    // make sure 404 is returned back
+    var newId = new ObjectID().toHexString();
+    request(app)
+    .patch(`/todos/${newId}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it('should return 404 for non-object ids',(done)=>{
+    request(app)
+    .patch('/todos/123')
+    .expect(404)
+    .end(done);
+  });
+
 });
