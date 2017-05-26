d190c36ee433d7fc297eb4e1597bc7161b1061ff
Naveen-Valmikam <naveen.valmikam@gmail.com>
2017-05-26 15:28:01 +1000

    Setup separate test and development configuration

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,6 @@
 var mongoose = require('mongoose');
-// let db = {
-//   localhost: 'mongodb://localhost:27017/TodoApp',
-//   mlab: 'mongodb://testuser:password1@ds153521.mlab.com:53521/todo-app-api'
-// };
-mongoose.connect( process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
+
+mongoose.connect( process.env.MONGODB_URI);
 
 mongoose.Promise = global.Promise;
 
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 
 const  express = require('express');
@@ -14,7 +16,7 @@ var {User} = require('./models/user');
 var app = express();
 app.use(bodyParser.json());
 
-const port = process.env.PORT||3000;
+const port = process.env.PORT;
 
 app.post('/todos',(req,res)=>{
   var todo = new Todo({
